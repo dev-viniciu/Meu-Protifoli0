210b232d209fa3cb890163f718438ab179e75e0c
LUIZ VINICIUS SOUSA SILVA <luiz_sousa-silva@estudante.sesisenai.org.br>
2026-09-09 17:07:16 -0300

    Primeiro Commit

---
+++ 00-demo/src/App.jsx
@@ -0,0 +1,35 @@
+import './App.css'
+
+function App() {
+ 
+  return (
+  <div className="cont-app">
+    <h1>Trabalhando com React</h1>
+    <h2>Posso trampar como dev</h2>
+    <p>Empresas e Software Houses: Consultorias e fábricas de software que desenvolvem aplicações sob demanda para terceiros.</p>
+    <br />
+     <p>Startups de Tecnologia: Ambientes dinâmicos que valorizam a agilidade do perfil técnico para construir MVPs (produtos mínimos viáveis) e novas funcionalidades.</p>
+     <br />
+     <p>Departamentos de TI e Inovação: Grandes empresas de setores tradicionais (bancos, varejo, saúde, indústria) que gerenciam equipes internacionais para cuidar de seus portais e sistemas.</p>
+     <br />
+     <p>Agências Digitais e de Marketing: Criação e manutenção de e-commerces, landing pages interativas, hotsites e blogs corporativos.</p>
+     <br />
+     <p>Setor Público: Órgãos governamentais e universidades por meio de estágios, contratos temporários ou concursos públicos para cargas técnicas.</p>
+
+     <h2>Trampo com tecnologia mas sem programação</h2>
+     <ul>
+     <li>Product Management & Negócios</li>
+     <br />
+     <li>Design e Experiência do Usuário (UI/UX)</li>
+     <br />
+     <li>Gestão de Projetos e Agilidade</li>
+     <br />
+     <li>Dados e Inteligência de Negócio</li>
+     <br />
+     <li>Vendas Técnicas e Sucesso do Cliente</li>
+     </ul>
+  </div>
+  )
+}
+
+export default App
