669464fa992ce735bc27e7c72d87ea4ae2883355
LUIZ VINICIUS SOUSA SILVA <luiz_sousa-silva@estudante.sesisenai.org.br>
2026-09-10 15:46:52 -0300

    Primeiro commit

--- 00-demo/src/App.jsx
+++ 00-demo/src/App.jsx
@@ -5,18 +5,20 @@ function App() {
   return (
   <div className="cont-app">
     <h1>Trabalhando com React</h1>
-    <h2>Posso trampar como dev</h2>
-    <p>Empresas e Software Houses: Consultorias e fábricas de software que desenvolvem aplicações sob demanda para terceiros.</p>
-    <br />
-     <p>Startups de Tecnologia: Ambientes dinâmicos que valorizam a agilidade do perfil técnico para construir MVPs (produtos mínimos viáveis) e novas funcionalidades.</p>
-     <br />
-     <p>Departamentos de TI e Inovação: Grandes empresas de setores tradicionais (bancos, varejo, saúde, indústria) que gerenciam equipes internacionais para cuidar de seus portais e sistemas.</p>
-     <br />
-     <p>Agências Digitais e de Marketing: Criação e manutenção de e-commerces, landing pages interativas, hotsites e blogs corporativos.</p>
-     <br />
-     <p>Setor Público: Órgãos governamentais e universidades por meio de estágios, contratos temporários ou concursos públicos para cargas técnicas.</p>
+    <h2>Onde posso trampar como dev ?</h2>
+    <ul>
+  <li><strong>Empresas e Software Houses:</strong> Consultorias e fábricas de software que desenvolvem aplicações sob demanda para terceiros.</li>
+  <br />
+  <li><strong>Startups de Tecnologia:</strong> Ambientes dinâmicos que valorizam a agilidade do perfil técnico para construir MVPs (produtos mínimos viáveis) e novas funcionalidades.</li>
+  <br />
+  <li><strong>Departamentos de TI e Inovação:</strong> Grandes empresas de setores tradicionais (bancos, varejo, saúde, indústria) que gerenciam equipes internacionais para cuidar de seus portais e sistemas.</li>
+  <br />
+  <li><strong>Agências Digitais e de Marketing:</strong> Criação e manutenção de e-commerces, landing pages interativas, hotsites e blogs corporativos.</li>
+  <br />
+  <li><strong>Setor Público:</strong> Órgãos governamentais e universidades por meio de estágios, contratos temporários ou concursos públicos para cargos técnicos.</li>
+</ul>
 
-     <h2>Trampo com tecnologia mas sem programação</h2>
+     <h2>Trampo com tecnologia mas sem programação ?</h2>
      <ul>
      <li>Product Management & Negócios</li>
      <br />
@@ -27,6 +29,20 @@ function App() {
      <li>Dados e Inteligência de Negócio</li>
      <br />
      <li>Vendas Técnicas e Sucesso do Cliente</li>
+     <br />
+     </ul>
+     
+     <h2>Vai fazer faculdade de outra coisa ? </h2>
+     <ul>
+     <li><strong>Direito:</strong> Resumos doutrinários, peças jurídicas e simulados.</li>
+     <br />
+     <li><strong>Saúde:</strong> Explicação de anatomia/fisiologia, artigos e casos clínicos.</li>
+     <br />
+     <li><strong>Negócios:</strong> Planos de negócios, análise SWOT e cálculos econômicos.</li>
+     <br />
+     <li><strong>Humanas:</strong> Resenhas, comparação de autores e normas ABNT.</li>
+     <br />
+     <li><strong>Engenharias:</strong> Passo a passo de cálculo, física e relatórios.</li>
      </ul>
   </div>
   )
