9d8c7b7cf6e7c9d47d7821873c400b0828ee48e3
LUIZ VINICIUS SOUSA SILVA <luiz_sousa-silva@estudante.sesisenai.org.br>
2026-09-23 17:02:28 -0300

    Salvo

--- studio-alfa/src/components/Header/Header.jsx
+++ studio-alfa/src/components/Header/Header.jsx
@@ -1,17 +1,27 @@
-function Header (){
-    return(
-        <header className = "header" >
-        <div
-            className = "logo">
-            <img className = "logo-icon"src="#" alt="logo"></img>
-            <span className="logo-text">Studio Alfa</span>
-        </div>
-        <nav className="nave"> Nave</nav>
+import "./Header.css";
+import logoImg from "../../assets/imgs/alfa.png";
+
+function Header() {
+  return (
+    <header className="header">
+      <div className="header-container">
+      <div className="logo">
+        <img className="logo-icon" src={logoImg} alt="Logo Studio Alfa" />
+        <span className="logo-text">Studio Alfa</span>
+      </div>
+
+      <nav className="nav">
         <a href="#">Inicio</a>
         <a href="#">Servicos</a>
         <a href="#">Sobre</a>
-        <a href="#" className="btn-contatos">Contato</a>
-        </header>
-    )
+        <a href="#" className="btn-contatos">
+          Contato
+        </a>
+      </nav>
+      </div>
+    </header>
+   
+  );
 }
-export default Header
+
+export default Header;
